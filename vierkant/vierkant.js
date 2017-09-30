db189d022c33c06e68a092aca92476d16ee4bf55
Simone <506356@edu.rocmn.nl>
2017-09-30 16:38:30 +0200

    Delete vierkant.js

--- vierkant/vierkant.js
+++
@@ -1,9 +0,0 @@
-function myFunction(){
-  var width = document.getElementById("width").value;
-  var height = document.getElementById("height").value;
-  var color = document.getElementById("color").value;
-  document.getElementById("square").style.width = width + "px";
-  document.getElementById("square").style.height = height + "px";
-  document.getElementById("square").style.backgroundColor = color;
-
-}
