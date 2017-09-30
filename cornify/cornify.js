96d2e5b6a63210173d7503b65687db6b5ba5e2d8
Simone <506356@edu.rocmn.nl>
2017-09-30 16:34:04 +0200

    Delete cornify.js

--- cornify/cornify.js
+++
@@ -1,8 +0,0 @@
-array = [];
-document.addEventListener("keypress", rainbow);
-function rainbow() {
-  array.push(String.fromCharCode(event.which || event.keyCode));
-     if(array.toString().replace(/,/g,'').includes("cornify")) {
-     cornify_add();
-    }
-}
