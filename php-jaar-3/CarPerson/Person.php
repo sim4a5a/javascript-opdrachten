cdd9560397583f3d95786c7787fa93996ef70bd6
Simone <506356@edu.rocmn.nl>
2017-11-22 17:47:12 +0100

    Add files via upload

---
+++ php-jaar-3/CarPerson/Person.php
@@ -0,0 +1,42 @@
+<?php
+
+
+class Person
+{
+    public $name;
+    private $age;
+    public function __construct($name) {
+        $this->name = $name;
+    }
+    public function showInfo() {
+        return 'Dit is ' . $this->name . ' en zijn/haar leeftijd is ' . $this->age;
+    }
+    public function setAge($age) {
+        if ($age >= 0 && $age <= 150) {
+            $this->age = $age;
+        }
+        return $age;
+    }
+    public function getAge($age) {
+        $this->age = $age;
+    }
+    public function adultOrNot() {
+        if ($this->age > 18) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+$tim = new Person('Tim', 21);
+echo $tim->showInfo();
+echo $tim->setAge(150);
+echo "<br>";
+$sharron = new Person('Shannon', 21);
+echo $sharron->showInfo();
+
+
+
+
+
+ ?>
