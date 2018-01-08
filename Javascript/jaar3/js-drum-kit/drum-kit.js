af9b157ca1c70cb0635c60877a7b20f6efe07177
Simone <506356@edu.rocmn.nl>
2018-01-08 15:06:05 +0100

    Add files via upload

---
+++ Javascript/jaar3/js-drum-kit/drum-kit.js
@@ -0,0 +1,11 @@
+
+document.addEventListener("keypress", function(event) {
+  var key = event.keyCode;
+  document.querySelector("audio[data-key='" + key +"']").play();
+  document.querySelector("div[data-key='" + key + "']").classList.toggle('playing');
+  setTimeout( function() {
+  document.querySelector("div[data-key='" + key + "']").classList.toggle('playing');
+}, 500);
+
+
+});
